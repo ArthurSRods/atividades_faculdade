556b354dba533b053b707b13449b1986359dd363
ArthurSRods <arthur.srodrigues@hotmail.com>
2024-09-12 22:33:20 -0300

    Modelo REACT - Projeto 02

---
+++ projeto-02/projeto-02/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Componente1 from './components/Componente1'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <Componente1></Componente1>
+    </>
+  )
+}
+
+export default App
